04453b4b2f36c23bdb9e9a784d6d4c55651e8f68
AmitThakkar <vigildbest@gmail.com>
2016-07-18 17:59:28 +0530

    Removing http provider error.

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,9 @@
 /**
  * Created by namita on 7/15/16.
  */
-
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-import { HTTP_PROVIDERS } from '@angular/http';
-bootstrap(AppComponent);
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+import {HTTP_PROVIDERS} from '@angular/http';
 //Applying the route configuration when bootstrapping Angular2 app
-bootstrap(AppComponent, [HTTP_PROVIDERS
-])
+bootstrap(<any>AppComponent, [HTTP_PROVIDERS])
     .catch(err => console.error(err));
